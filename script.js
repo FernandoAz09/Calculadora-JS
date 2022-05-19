6b9e89e436e691d00a8c2b973309c619c0662157
Fernando <fsazevedo09@gmail.com>
2022-05-18 21:34:27 -0300

    usando eval() para calcular expressões

--- script.js
+++ script.js
@@ -5,9 +5,26 @@ function insert(num) {
     document.getElementById('resultado').innerHTML = numero + num
 }
 
-
 /* ---- Apaga tudo CA (clear all) ---- */
 
 function clearAll() {
     document.getElementById('resultado').innerHTML = ""
+}
+
+/* ---- Apaga ultimo, backspace ---- */
+
+function backspace() {
+    var back = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = back.substring(0, back.length - 1)
+}
+
+/* ---- Calcular ---- */
+
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado) //EVAL é uma bosta -.-"
+    }else{
+        document.getElementById('resultado').innerHTML = "nada..."
+    }
 }
